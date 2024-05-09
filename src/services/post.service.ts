283d1856255cf0e94d295533475addfa8621f2ed
wickedside <w1ck3ds1d3@gmail.com>
2024-05-09 22:35:22 +0300

    eslintrc updated to work with TypeScript, services migrated

---
+++ src/services/post.service.ts
@@ -0,0 +1,25 @@
+import * as postRepository from '../repositories/memory.repository';
+import Post from '../models/post.model';
+
+export const getAllPosts = (): Post[] => {
+  return postRepository.getPosts();
+};
+
+export const getPostById = (id: string): Post | undefined => {
+  return postRepository.findPostById(id);
+};
+
+export const createPost = (postData: Post): Post => {
+  return postRepository.addPost(postData);
+};
+
+export const updatePost = (id: string, updateData: Partial<Post>): Post | null => {
+  const post = postRepository.findPostById(id);
+  if (!post) return null;
+  Object.assign(post, updateData);
+  return post;
+};
+
+export const deletePost = (id: string): void => {
+  postRepository.deletePost(id);
+};
