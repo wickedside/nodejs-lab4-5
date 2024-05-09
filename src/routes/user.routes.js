767468da394b8db1c435167b39b007398d50228e
wickedside <w1ck3ds1d3@gmail.com>
2024-05-09 20:58:06 +0300

    made user router and user controller

---
+++ src/routes/user.routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import * as UserController from '../controllers/user.controller.js';
+
+const router = Router();
+
+router.get('/', UserController.getAllUsers);
+router.get('/:id', UserController.getUserById);
+router.post('/', UserController.createUser);
+router.put('/:id', UserController.updateUser);
+router.delete('/:id', UserController.deleteUser);
+
+export default router;
